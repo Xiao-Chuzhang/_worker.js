5f95088fbee8a0856cea993489dcd30e349b9602
Xiao-Chuzhang <103270199+Xiao-Chuzhang@users.noreply.github.com>
2023-02-25 23:34:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,9 @@
-addEventListener(
-  "fetch",event => {
-     let url=new URL(event.request.url);
-     url.hostname="www.github.com";  //你需要反代的域名
-     let request=new Request(url,event.request);
-     event. respondWith(
-       fetch(request)
-     )
-  }
-)
+export default {
+  async fetch(request, env) {
+    const _url = new URL(request.url);
+    _url.hostname = _url.pathname.startsWith("/gh/")
+      : "www.github.com";
+    const req = new Request(_url, request);
+    return fetch(req);
+  },
+};
